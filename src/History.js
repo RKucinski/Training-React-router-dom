61f3e39c92618fc935d111c8bde6b22e741b59a4
Roman Kucinski <roman.kucinski@gmail.com>
2018-10-08 10:49:32 +0200

    first commit

---
+++ src/History.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class History extends Component{
+    render(){
+        return(
+            <div>
+                <p>Notre histoire</p>
+            </div>
+        )
+    }
+}
+
+export default History
